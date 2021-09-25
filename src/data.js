e9f9e629b9e95306761b82ab49dc106d0c0f8879
dzuhriandika <dzuhriandika@gmail.com>
2021-09-25 14:45:38 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const projects = [
+  {
+    title: "Tavern",
+    subtitle: "react-native",
+    description:
+      "Tavern is a restaurant admin application. where in this application we can receive orders, update food menus, receive top ups, edit food menus, and calculate the amount of income for one month.",
+    image: "./project-2.png",
+    link: "https://github.com/dzuhriandika/Tavern-Admin",
+  },
+  {
+    title: "Soemantryzhop",
+    subtitle: "express nodejs mysql",
+    description:
+      "Soemantryzhop is personal marketplace website. On this website we can see about the owner, galery owner, and order goods on this website.",
+    image: "./project-1.png",
+    link: "https://github.com/dzuhriandika/express-Soemantryzhop",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React Native",
+  "Git",
+  "Linux",
+  "Laravel",
+  "Node JS",
+];
